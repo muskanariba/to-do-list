b960ce8f85414b6fabc6b58e30751beec7f88db2
muskanariba <159161028+muskanariba@users.noreply.github.com>
2024-10-01 01:11:00 +0500

    Add files via upload

---
+++ to do list/app.js
@@ -0,0 +1,23 @@
+// function addTask() {
+//     const task = document.getElementById("taskInput").value;
+//   if(task==""){
+//     alert("please enter task")
+//     return
+//   }
+
+//     const li = document.createElement("li");
+//     li.innerHTML = task + ' <button onclick="this.parentElement.remove()" class="delete" >Delete</button>';
+//     document.getElementById("taskList").appendChild(li);
+//     document.getElementById("taskInput").value = "";}
+
+function addTask(){
+const task = document.getElementById("taskInput").value;
+if(task == ""){
+    alert("please enter task")
+        return
+}
+const li = document.createElement("li")
+li.innerHTML = task + `<button onclick = this .parentElement.remove() class="delete" >Delete</button>`;
+document.getElementById('taskList').appendChild(li)
+document.getElementById("taskInput").value = "";
+}
